dc0d4cc24f4c96eac306883eb39d121f228c625d
survikrowa <szymonhrabia60@gmail.com>
2022-06-22 00:51:47 +0200

    feat/add-prettier-and-eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
-import { defineNuxtConfig } from "nuxt";
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+  typescript: {
+    strict: true,
+  },
 });
